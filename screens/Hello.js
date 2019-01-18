b37070fd0c587ab9a25132ad8d632ffdc31d31d1
unknown <fabio.soares@live.fr>
2019-01-18 19:00:42 +0100

    cards

--- screens/Hello.js
+++ screens/Hello.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Animated, TouchableWithoutFeedback, StyleSheet, View, Text } from 'react-native';
-//let time
+import cards from '../assets/locales/fr/cards'
 
 export default class Hello extends React.Component {
   constructor(props){
       super(props)
       this.state = {
-          time: 0          
+          time: 0,
+          card: {}          
       }
       //this.time = '';
   }
@@ -21,13 +22,17 @@ export default class Hello extends React.Component {
   }
   timing = () => {
     this.setState((prevState) => ({
-        time: prevState.time + 1
+        time: prevState.time >= 78 ? 0 : prevState.time + 1
     })); 
   }
   handlePressOut(e){
     clearInterval(this.time)
+    this.setState({
+        card: cards.tarot_interpretations[this.state.time]
+    })
   }
   render() {
+      //console.log('length : ', cards.tarot_interpretations.length)
     return (
       <View style={styles.container}>
         <View style={styles.message}>
@@ -36,11 +41,13 @@ export default class Hello extends React.Component {
         </View>
         <View style={styles.container_btn}>
             <TouchableWithoutFeedback onPressIn={this.onPress.bind(this)} onPressOut={this.handlePressOut.bind(this)}>
-            <View style={styles.button} >
-            <Text> Démarrer </Text>
-            </View>
-                
+                <View style={styles.button} >
+                    <Text> Démarrer </Text>
+                </View>                
             </TouchableWithoutFeedback>
+        </View>
+        <View style={styles.card}>
+            <Text> {this.state.card && this.state.card.name } </Text>
         </View>
       </View>
     );
